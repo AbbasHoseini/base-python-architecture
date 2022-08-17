e7418335c7b9dd5de638f0975e59f9ce88339065
AbbasHoseini <abbashoseini.ir@gmail.com>
2022-08-17 17:56:47 +0430

    test

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
 
-# main
-# python --version
-def main():
-    print('Hi there')
 
 
+x = 2
+
+# if 5 > 2:
+#     print('5 greater than 2')
+# if 6 == 6:
+#     print('second if')
+# print('hahaha')
+# x = 5
+# y = 'This is string'
+# print('x is: ', x, 'type:', type(x))
+# print('y is: ', y, 'type:', type(y))
+# z = None
+# print('type of none value in python ', type(z))
+# z = 300
+# print('z after give value', z, 'type:', type(z))
+# THIS IS COMMENT
+
+""" this is multiple comment """
+
+# x = float(3)
+# print('x is: ', x, 'type:', type(x))
+# x = -2j
+# print(x)
 
 
+print(x)
 
 
-main()
